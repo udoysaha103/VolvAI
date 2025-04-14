90d99677128d579475c43ca91402e18b40d04ee0
udoysaha103 <udoysaha103@gmail.com>
2025-04-14 22:06:30 +0600

    change port

--- backend/server.js
+++ backend/server.js
@@ -97,7 +97,7 @@ app.get('/get-messages', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
